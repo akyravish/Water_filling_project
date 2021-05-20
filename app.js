4c8e40ee6b8da7a1ebec064cffa6407e8863edaa
akyravish <info.akshayravish@gmail.com>
2021-05-20 10:51:31 +0530

    add more line code

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 // jshint esversion:6
 
 const smallCups = document.querySelectorAll('.cup-small');
-const remained = document.getElementById('.remained');
-const liters = document.getElementById('.liters');
-const percentage = document.getElementById('.percentage');
+const remained = document.getElementById('remained');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+
+const updateBigCup = () => {
+	const fullCups = document.querySelectorAll('.cup-small.full').length;
+	const totalCups = smallCups.length;
+
+	if (fullCups === '0') {
+		percentage.style.opacity = 0;
+		percentage.style.height = 0;
+	} else {
+		percentage.style.opacity = 1;
+		percentage.style.height = `${(fullCups / totalCups) * 30}rem`;
+		percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+	}
+
+	if (fullCups === totalCups) {
+		remained.style.opacity = 0;
+		remained.style.height = 0;
+	} else {
+		remained.style.opacity = 1;
+		liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+	}
+};
+
+updateBigCup();
 
 const highLightCup = (idx) => {
+	if (
+		smallCups[idx].classList.contains('full') &&
+		!smallCups[idx].nextElementSibling?.classList.contains('full')
+	) {
+		idx--;
+	}
+
 	smallCups.forEach((cup, idx2) => {
 		if (idx2 <= idx) {
 			cup.classList.add('full');
@@ -13,6 +44,8 @@ const highLightCup = (idx) => {
 			cup.classList.remove('full');
 		}
 	});
+
+	updateBigCup();
 };
 
 smallCups.forEach((cup, idx) => {
